a3ef1f53f3fa89d654dd23522d869a1defb15c61
AlexKonstantinoff <85986748+AlexKonstantinoff@users.noreply.github.com>
2021-11-01 12:02:06 +0300

    Commit_01_11

---
+++ Find_primes_optimized_memory.py
@@ -0,0 +1,25 @@
+
+
+def find_primes_optimized_memory(f_num):
+    primes = [2]
+
+    for i in range(3, f_num, 2):
+        flag = True
+        for j in primes:
+            if j**2 > i:
+                break
+            if i % j == 0:
+                flag = False
+                break
+        if flag:
+            primes.append(i)
+
+    return primes
+    
+
+
+f = int(input('Введите конечное число: '))
+res = find_primes_optimized_memory(f)
+
+print(res)
+
