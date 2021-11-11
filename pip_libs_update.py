e41c51b13dc7a2227f3c37814cc5a4b70c5934e1
AlexKonstantinoff <85986748+AlexKonstantinoff@users.noreply.github.com>
2021-11-11 07:10:53 +0300

    Package_updater

---
+++ pip_libs_update.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist 
+    in pkg_resources.working_set]
+
+for p in packages:
+    if p.__contains__('~'):
+        packages.pop(p)
+call("pip install --upgrade " + ' '.join(packages), shell=True)
